7dbf926990e69eb230c3ce6f32a8dad8038be65e
Huu Tai <tainguyenzz18@gmail.com>
2021-05-27 20:50:27 +0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.textBoxSoLg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxMa = new System.Windows.Forms.TextBox();
+            this.buttonThoat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonThem = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTen = new System.Windows.Forms.TextBox();
+            this.textBoxDonGia = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.hinh_nen_slide_thuyet_trinh_dep_24;
+            this.panelMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panelMenu.Controls.Add(this.buttonClear);
+            this.panelMenu.Controls.Add(this.label6);
+            this.panelMenu.Controls.Add(this.textBoxSoLg);
+            this.panelMenu.Controls.Add(this.label3);
+            this.panelMenu.Controls.Add(this.textBoxMa);
+            this.panelMenu.Controls.Add(this.buttonThoat);
+            this.panelMenu.Controls.Add(this.label2);
+            this.panelMenu.Controls.Add(this.dataGridView1);
+            this.panelMenu.Controls.Add(this.label4);
+            this.panelMenu.Controls.Add(this.label5);
+            this.panelMenu.Controls.Add(this.buttonThem);
+            this.panelMenu.Controls.Add(this.buttonEdit);
+            this.panelMenu.Controls.Add(this.buttonDelete);
+            this.panelMenu.Controls.Add(this.label1);
+            this.panelMenu.Controls.Add(this.textBoxTen);
+            this.panelMenu.Controls.Add(this.textBoxDonGia);
+            this.panelMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelMenu.Location = new System.Drawing.Point(3, 9);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(815, 606);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // textBoxSoLg
+            // 
+            this.textBoxSoLg.Location = new System.Drawing.Point(227, 176);
+            this.textBoxSoLg.Name = "textBoxSoLg";
+            this.textBoxSoLg.Size = new System.Drawing.Size(150, 34);
+            this.textBoxSoLg.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(99, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Số Lượng:";
+            // 
+            // textBoxMa
+            // 
+            this.textBoxMa.Enabled = false;
+            this.textBoxMa.Location = new System.Drawing.Point(227, 119);
+            this.textBoxMa.Name = "textBoxMa";
+            this.textBoxMa.Size = new System.Drawing.Size(150, 34);
+            this.textBoxMa.TabIndex = 5;
+            // 
+            // buttonThoat
+            // 
+            this.buttonThoat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonThoat.BackgroundImage")));
+            this.buttonThoat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonThoat.Location = new System.Drawing.Point(489, 239);
+            this.buttonThoat.Name = "buttonThoat";
+            this.buttonThoat.Size = new System.Drawing.Size(100, 31);
+            this.buttonThoat.TabIndex = 13;
+            this.buttonThoat.UseVisualStyleBackColor = true;
+            this.buttonThoat.Click += new System.EventHandler(this.buttonThoat_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Mã:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.MenuText;
+            this.dataGridView1.Location = new System.Drawing.Point(104, 319);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(605, 254);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(435, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 29);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Đơn Giá:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(435, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Tên:";
+            // 
+            // buttonThem
+            // 
+            this.buttonThem.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.icons8_add_48px1;
+            this.buttonThem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonThem.Location = new System.Drawing.Point(171, 239);
+            this.buttonThem.Name = "buttonThem";
+            this.buttonThem.Size = new System.Drawing.Size(100, 31);
+            this.buttonThem.TabIndex = 2;
+            this.buttonThem.UseVisualStyleBackColor = true;
+            this.buttonThem.Click += new System.EventHandler(this.buttonThem_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.icons8_edit_64px_1;
+            this.buttonEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonEdit.Location = new System.Drawing.Point(277, 239);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(100, 31);
+            this.buttonEdit.TabIndex = 3;
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.Transparent;
+            this.buttonDelete.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.icons8_delete_64px;
+            this.buttonDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDelete.Location = new System.Drawing.Point(383, 239);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(100, 31);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(248, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quan Li Thuc An";
+            // 
+            // textBoxTen
+            // 
+            this.textBoxTen.Location = new System.Drawing.Point(559, 121);
+            this.textBoxTen.Name = "textBoxTen";
+            this.textBoxTen.Size = new System.Drawing.Size(150, 34);
+            this.textBoxTen.TabIndex = 8;
+            // 
+            // textBoxDonGia
+            // 
+            this.textBoxDonGia.Location = new System.Drawing.Point(559, 179);
+            this.textBoxDonGia.Name = "textBoxDonGia";
+            this.textBoxDonGia.Size = new System.Drawing.Size(150, 34);
+            this.textBoxDonGia.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(99, 287);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Thông Tin";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonClear.BackgroundImage")));
+            this.buttonClear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonClear.Location = new System.Drawing.Point(595, 239);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(100, 31);
+            this.buttonClear.TabIndex = 15;
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 609);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonThem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonThoat;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxTen;
+        private System.Windows.Forms.TextBox textBoxDonGia;
+        private System.Windows.Forms.TextBox textBoxSoLg;
+        private System.Windows.Forms.TextBox textBoxMa;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string conectionString ;
+        private SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conectionString = @"Data Source=NHT;Initial Catalog=QuanLiCF2;Integrated Security=True";
+            connection = new SqlConnection(conectionString);
+            // mở kết nối
+            connection.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            //load dữ liệu vào bảng
+            dataGridView1.DataSource = getFood().Tables[0];
+        }
+
+        // load dữ liệu từ database
+        DataSet getFood()
+        {
+            String query = "SELECT * FROM Food";
+
+            DataSet data = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            adapter.Fill(data);
+            return data;
+        }
+
+        // kiểm tra thông tin
+        private bool checkThongTin()
+        {
+            if(textBoxTen.Text == "" || textBoxDonGia.Text == "" || textBoxSoLg.Text == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // bắt sự kiện click vào bảng
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // lấy index của hàng đang chọn
+            int index = e.RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[index];
+
+            //get dữ liệu của hàng vào textbox
+            textBoxMa.Text = selectedRow.Cells[0].Value.ToString();
+            textBoxTen.Text = selectedRow.Cells[1].Value.ToString();
+            textBoxDonGia.Text = selectedRow.Cells[2].Value.ToString();
+            textBoxSoLg.Text = selectedRow.Cells[3].Value.ToString();
+        }
+
+        //thêm dữ liệu
+        private void buttonThem_Click(object sender, EventArgs e)
+        {
+            // Kiem tra đã đầy đủ thông tin chưa
+            if (!checkThongTin())
+            {
+                MessageBox.Show("Vui Lòng Điền Đầy Đủ Thông Tin");
+                return;
+            }
+
+            string query = "INSERT INTO Food VALUES(@name,@price,@amount)";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            // truyền tham số vào query
+            command.Parameters.AddWithValue("@name", textBoxTen.Text);
+            command.Parameters.AddWithValue("@price", textBoxDonGia.Text);
+            command.Parameters.AddWithValue("@amount", textBoxSoLg.Text);
+            // thực hiện câu query
+            command.ExecuteNonQuery();
+
+            // load lại data vào bảng
+            dataGridView1.DataSource = getFood().Tables[0];
+
+            // cho cac text bằng rỗng
+            textBoxMa.Text = "";
+            textBoxTen.Text = "";
+            textBoxSoLg.Text = "";
+            textBoxDonGia.Text = "";
+        }
+
+        //xoá dữ liệu
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            // Kiem tra đã chọn thức ăn chưa
+            if (!checkThongTin())
+            {
+                MessageBox.Show("Vui Lòng Chọn Thức Ăn Cần Xoá");
+                return;
+            }
+
+            string query = "DELETE FROM Food WHERE id=@id;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            // truyền tham số vào query
+            command.Parameters.AddWithValue("@id", textBoxMa.Text);
+            // thực hiện câu query
+            command.ExecuteNonQuery();
+
+            // load lại data vào bảng
+            dataGridView1.DataSource = getFood().Tables[0];
+
+            // cho cac text bằng rỗng
+            textBoxMa.Text = "";
+            textBoxTen.Text = "";
+            textBoxSoLg.Text = "";
+            textBoxDonGia.Text = "";
+        }
+
+        //thoát
+        private void buttonThoat_Click(object sender, EventArgs e)
+        {
+            // ngắt kết nối
+            connection.Close();
+            //đóng chương trình
+            Application.Exit();
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            // Kiem tra đã đầy đủ thông tin chưa
+            if (!checkThongTin())
+            {
+                MessageBox.Show("Vui Lòng Điền Đầy Đủ Thông Tin");
+                return;
+            }
+
+            string query = "UPDATE Food SET name=@name,price=@price,amount=@amount WHERE id=@id;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            // truyền tham số vào query
+            command.Parameters.AddWithValue("@id", textBoxMa.Text);
+            command.Parameters.AddWithValue("@name", textBoxTen.Text);
+            command.Parameters.AddWithValue("@price", textBoxDonGia.Text);
+            command.Parameters.AddWithValue("@amount", textBoxSoLg.Text);
+            // thực hiện câu query
+            command.ExecuteNonQuery();
+
+            // load lại data vào bảng
+            dataGridView1.DataSource = getFood().Tables[0];
+
+            // cho cac text bằng rỗng
+            textBoxMa.Text = "";
+            textBoxTen.Text = "";
+            textBoxSoLg.Text = "";
+            textBoxDonGia.Text = "";
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // cho cac text bằng rỗng
+            textBoxMa.Text = "";
+            textBoxTen.Text = "";
+            textBoxSoLg.Text = "";
+            textBoxDonGia.Text = "";
+        }
+    }
+}
